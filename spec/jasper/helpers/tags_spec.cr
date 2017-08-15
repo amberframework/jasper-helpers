01bd659e44a7ca707b1269fe658e7bdf311f321c
Isaac Sloan <isaac@isaacsloan.com>
2017-08-15 00:29:31 -0600

    Working form_tags (#10)
    * added early link_to helpers.
    
    * added early button_to implementation.
    
    * fixed missing new line.
    
    * fixed some formatting.
    
    * WiP on forms.
    
    * cleaned up form, and added a hidden field with the method type when not a POST.
    
    * little bit cleaner.
    
    * added csrf field inside of form. TODO: add in macro for setting the token.
    
    * Fixed the select_field helpers
    
    * cleaning up
    
    * meh

--- spec/jasper/helpers/tags_spec.cr
+++ spec/jasper/helpers/tags_spec.cr
@@ -21,12 +21,12 @@ describe Jasper::Helpers::Tags do
 
   describe "#content" do
     it "accepts a content string" do
-      content(name: :span, options: { :id => "bar" }, content: "Hello").should eq("<span id=\"bar\">Hello</span>")
+      content(element_name: :span, options: { :id => "bar" }, content: "Hello").should eq("<span id=\"bar\">Hello</span>")
     end
 
     it "accepts a block as input" do
-      result = content(name: :div, options: { :id => "foo" }) do
-        content(name: :span, options: { :id => "bar" }, content: "Hello")
+      result = content(element_name: :div, options: { :id => "foo" }) do
+        content(element_name: :span, options: { :id => "bar" }, content: "Hello")
       end
 
       result.should eq("<div id=\"foo\"><span id=\"bar\">Hello</span></div>")
