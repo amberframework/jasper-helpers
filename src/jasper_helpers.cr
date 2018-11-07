69686d6ed1143de16d3db00c225e61872acc32e7
Aaron Namba <aaron@aaronnamba.com>
2018-11-07 16:47:05 +0900

    select_field: allow an array of selected values

--- src/jasper_helpers.cr
+++ src/jasper_helpers.cr
@@ -1,7 +1,7 @@
 require "./jasper_helpers/*"
 
 module JasperHelpers
-  alias OptionHash = Hash(Symbol, Nil | String | Symbol | Bool | Int8 | Int16 | Int32 | Int64 | Float32 | Float64 | Time | Bytes)
+  alias OptionHash = Hash(Symbol, Nil | String | Symbol | Bool | Int8 | Int16 | Int32 | Int64 | Float32 | Float64 | Time | Bytes | Array(String))
 
   include Tags
   include Forms
