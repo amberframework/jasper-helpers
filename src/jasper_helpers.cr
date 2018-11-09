0b9d1fbbb9423cffb98cdc3716df430794b66537
Aaron Namba <aaron@aaronnamba.com>
2018-11-09 09:10:49 +0900

    select_field: selected value can now be one of Int32, String, Array(String), Array(Int32), Array(String | Int32)

--- src/jasper_helpers.cr
+++ src/jasper_helpers.cr
@@ -1,7 +1,7 @@
 require "./jasper_helpers/*"
 
 module JasperHelpers
-  alias OptionHash = Hash(Symbol, Nil | String | Symbol | Bool | Int8 | Int16 | Int32 | Int64 | Float32 | Float64 | Time | Bytes | Array(String))
+  alias OptionHash = Hash(Symbol, Nil | String | Symbol | Bool | Int8 | Int16 | Int32 | Int64 | Float32 | Float64 | Time | Bytes | Array(String) | Array(Int32) | Array(String | Int32))
 
   include Tags
   include Forms
